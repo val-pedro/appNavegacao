02c1286684f3a262214f73058a0a488b093eff6f
valdemir.pedro <valdemir.pedro@sp.senai.br>
2024-06-07 15:23:50 -0300

    cria a navegacao tabs com icones

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator  } from '@react-navigation/bottom-tabs';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 import HomeScreen from "./views/HomeScreen";
 import SobreScreen from "./views/SobreScreen";
 import ProdutosScreen from "./views/ProdutosScreen";
 import ContatoScreen from "./views/ProdutosScreen";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Sobre" component={SobreScreen} />
-                <Stack.Screen name="Produtos" component={ProdutosScreen} />
-                <Stack.Screen name="Contato" component={ContatoScreen} />
-            </Stack.Navigator>
+            <Tab.Navigator initialRouteName='Home'>
+                <Tab.Screen name="Home" component={HomeScreen} 
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={size} />
+                    ),
+                }}
+                />
+
+                <Tab.Screen name="Sobre" component={SobreScreen} />
+                <Tab.Screen name="Produtos" component={ProdutosScreen} />
+                <Tab.Screen name="Contato" component={ContatoScreen} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
